6418307d0c2ccd7a23d92bc61d0e3f6b774b0f59
Denis <inquis87@gmail.com>
2022-11-29 21:50:22 +0300

    недели

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите номер дня недели Д = ");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 3)
+
+{
+    Console.WriteLine("День недели Среда");
+
+
+}
+if (a == 5)
+{
+    Console.Write("День недели Пятница");
+}
